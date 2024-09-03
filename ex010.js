be8577c46b484d150b2313ef6143966646cacb39
Davi Diniz <vicentedavid31@outlook.com>
2024-09-03 19:24:18 -0300

    shortened code
    The code was shortened in some tests and I managed to find a way to get the same result with fewer lines.

--- ex010.js
+++ ex010.js
@@ -11,20 +11,13 @@ Times.innerHTML = `<p>O Horario atual é ${hour} horas e ${mins} minutos.</p>`
 
 if (hour >= 6 && hour < 12) {
     bg.style.backgroundColor = 'lightblue'
-}
-else if (hour >= 12 && hour < 18) {
-    bg.style.backgroundColor = 'lightyellow'
-}
-else { 
-    bg.style.backgroundColor = '#42426F'
-}
-
-if (hour >= 6 && hour < 12) {
     image.innerHTML = '<img src="imgs/caribe-brasileiro-capa-1-820x430.png" alt="Morning"></img>'
 }
 else if (hour >= 12 && hour < 18) {
+    bg.style.backgroundColor = 'lightyellow'
     image.innerHTML = '<img src="imgs/depositphotos_83681132-stock-photo-sunset-on-the-beach.jpg" alt="Afternoon"></img>'
 }
-else {
+else { 
+    bg.style.backgroundColor = '#42426F'
     image.innerHTML = '<img src="imgs/luar-na-praia-das-ondas-ao-anoitecer_506134-6525.png">'
 }
